0d6ad46541638db8cca06fd57153030a14036fa3
Kavkazi <shalulov127@gmail.com>
2022-01-10 01:12:49 +0200

    uploading 1st time code project סיימתי את העבודה אבל אני לא מבין למה זה לא עובד וזה פשוט קורס שאני עושה SAVE .

--- app/src/main/java/com/example/practice_dialog_app/MainActivity.java
+++ app/src/main/java/com/example/practice_dialog_app/MainActivity.java
@@ -3,31 +3,89 @@ package com.example.practice_dialog_app;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.text.DateFormat;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener{
-
+    SharedPreferences sp;
+    EditText citeEditText,lastNameEditText,firstNameEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Button btn_datepicker,btn_save;
+        Context context = null;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        btn_datepicker = (Button) findViewById(R.id.btn_datePiker);
+        btn_datepicker.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 DialogFragment datePicker = new DatePickerFragment();
                 datePicker.show(getSupportFragmentManager(), "date picker");
             }
         });
+        firstNameEditText=findViewById(R.id.firstNameEditText);
+        lastNameEditText=findViewById(R.id.lastNameEditText);
+        citeEditText= findViewById(R.id.citeEditText);
+        sp = getSharedPreferences("details1",0);
+
+        String strfirstNameEditText=sp.getString("firstNameEditText",null);
+        String strLastNameEditText=sp.getString("lastNameEditText",null);
+        String strCiteEditText=sp.getString("citeEditText",null);
+        if (strfirstNameEditText!=null&&lastNameEditText!=null&&citeEditText!=null){
+            firstNameEditText.setHint(strfirstNameEditText);
+            lastNameEditText.setHint(strLastNameEditText);
+            citeEditText.setHint(strCiteEditText);
+        }
+
+
+        btn_save = (Button) findViewById(R.id.btn_save);
+        btn_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                AlertDialog.Builder builder1 = new AlertDialog.Builder(context);
+                builder1.setMessage("are you shure you want to save all the data?");
+                builder1.setCancelable(true);
+
+                builder1.setPositiveButton(
+                        "Yes",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                SharedPreferences.Editor editor=sp.edit();
+                                editor.putString("firstNameEditText",strfirstNameEditText.toString());
+                                editor.putString("lastNameEditText",strLastNameEditText.toString());
+                                editor.putString("citeEditText",strCiteEditText.toString());
+                                editor.commit();
+                            }
+                        });
+
+                builder1.setNegativeButton(
+                        "No",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+                AlertDialog alert11 = builder1.create();
+                alert11.show();
+            }
+        });
+
+
     }
 
     @Override
@@ -38,7 +96,8 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
         c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
         String currentDateString = DateFormat.getDateInstance(DateFormat.FULL).format(c.getTime());
 
-        TextView textView = (TextView) findViewById(R.id.textView);
-        textView.setText(currentDateString);
+        Button btn_datepicker = (Button) findViewById(R.id.btn_datePiker);
+        btn_datepicker.setText(currentDateString);
+
     }
 }
